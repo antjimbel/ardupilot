e373cf2e4f1f2622612376a04bbb4a290151bbcf
Antonio Jimenez Bellido <ajbellido.es@gmail.com>
2019-11-03 09:57:14 +0100

    fix external i2c compass enumeration for APs with low resources

--- libraries/AP_Compass/AP_Compass.cpp
+++ libraries/AP_Compass/AP_Compass.cpp
@@ -672,7 +672,6 @@ void Compass::_probe_external_i2c_compasses(void)
         }
     }
     
-#if !HAL_MINIMIZE_FEATURES
     // AK09916 on ICM20948
     FOREACH_I2C_EXTERNAL(i) {
         ADD_BACKEND(DRIVER_ICM20948, AP_Compass_AK09916::probe_ICM20948(GET_I2C_DEVICE(i, HAL_COMPASS_AK09916_I2C_ADDR),
@@ -750,7 +749,6 @@ void Compass::_probe_external_i2c_compasses(void)
         ADD_BACKEND(DRIVER_IST8308, AP_Compass_IST8308::probe(GET_I2C_DEVICE(i, HAL_COMPASS_IST8308_I2C_ADDR),
                                                               true, ROTATION_NONE));
     }
-#endif // HAL_MINIMIZE_FEATURES
 }
 
 /*
